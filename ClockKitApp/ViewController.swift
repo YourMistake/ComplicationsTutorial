05fddde82f4f28dfd7b583321db89ed4b90d42ac
Laura Vanlandingham <Claudia@claudia.iwunet.indwes.edu>
2015-12-06 12:46:18 -0500

    Initial Commit

---
+++ ClockKitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClockKitApp
+//
+//  Created by Caudia on 12/6/15.
+//  Copyright © 2015 Caudia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
