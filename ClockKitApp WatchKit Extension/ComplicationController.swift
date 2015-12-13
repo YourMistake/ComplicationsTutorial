f5edd3268210762ca7c2d6bce85d6a27b33d961d
YourMistake <erik.vanlandingham@myemail.indwes.edu>
2015-12-13 13:54:03 -0500

    Wearables Vanlandingham Complications

--- ClockKitApp WatchKit Extension/ComplicationController.swift
+++ ClockKitApp WatchKit Extension/ComplicationController.swift
@@ -1,3 +1,5 @@
+//  Erik Vanlandingham
+//  Complications Tutorial
 //
 //  ComplicationController.swift
 //  ClockKitApp WatchKit Extension
@@ -11,18 +13,55 @@ import ClockKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
-    // MARK: - Timeline Configuration
+    let timeLineText = ["John3:16", "2Cor", "Eph", "Rev"]
+    
+    func getPlaceholderTemplateForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTemplate?) -> Void) {
+        
+        let template = CLKComplicationTemplateModularLargeStandardBody()
+        let icon = UIImage(named: "nine")
+        
+        template.headerImageProvider =
+            CLKImageProvider(onePieceImage: icon!)
+        
+        template.headerTextProvider =
+            CLKSimpleTextProvider(text: "Bible Festival")
+        template.body1TextProvider =
+            CLKSimpleTextProvider(text: "Bible Verses")
+        
+        handler(template)
+    }
+    
+    func createTimeLineEntry(headerText: String, bodyText: String, date: NSDate) -> CLKComplicationTimelineEntry {
+        
+        let template = CLKComplicationTemplateModularLargeStandardBody()
+        let icon = UIImage(named: "nine")
+        
+        template.headerImageProvider =
+            CLKImageProvider(onePieceImage: icon!)
+        template.headerTextProvider = CLKSimpleTextProvider(text: headerText)
+        template.body1TextProvider = CLKSimpleTextProvider(text: bodyText)
+        
+        let entry = CLKComplicationTimelineEntry(date: date,
+            complicationTemplate: template)
+        
+        return(entry)
+    }
     
     func getSupportedTimeTravelDirectionsForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.Forward, .Backward])
+        handler([.Forward])
     }
     
     func getTimelineStartDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
-        handler(nil)
+        let currentDate = NSDate()
+        handler(currentDate)
     }
     
     func getTimelineEndDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
-        handler(nil)
+        let currentDate = NSDate()
+        let endDate =
+        currentDate.dateByAddingTimeInterval(NSTimeInterval(4 * 60 * 60))
+        
+        handler(endDate)
     }
     
     func getPrivacyBehaviorForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationPrivacyBehavior) -> Void) {
@@ -32,8 +71,18 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntryForComplication(complication: CLKComplication, withHandler handler: ((CLKComplicationTimelineEntry?) -> Void)) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        if complication.family == .ModularLarge {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "hh:mm"
+            
+            let timeString = dateFormatter.stringFromDate(NSDate())
+            
+            let entry = createTimeLineEntry(timeString, bodyText: timeLineText[0], date: NSDate())
+            
+            handler(entry)
+        } else {
+            handler(nil)
+        }
     }
     
     func getTimelineEntriesForComplication(complication: CLKComplication, beforeDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
@@ -42,8 +91,26 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     func getTimelineEntriesForComplication(complication: CLKComplication, afterDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        
+        var timeLineEntryArray = [CLKComplicationTimelineEntry]()
+        var nextDate = NSDate(timeIntervalSinceNow: 1 * 60 * 60)
+        
+        let arrayLength = timeLineText.count // technically this should work?
+        
+        for index in 1...arrayLength {
+            
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "hh:mm"
+            
+            let timeString = dateFormatter.stringFromDate(nextDate)
+            
+            let entry = createTimeLineEntry(timeString, bodyText: timeLineText[index], date: nextDate)
+            
+            timeLineEntryArray.append(entry)
+            
+            nextDate = nextDate.dateByAddingTimeInterval(1 * 60 * 60)
+        }
+        handler(timeLineEntryArray)
     }
     
     // MARK: - Update Scheduling
@@ -54,10 +121,5 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     // MARK: - Placeholder Templates
-    
-    func getPlaceholderTemplateForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
-    }
     
 }
